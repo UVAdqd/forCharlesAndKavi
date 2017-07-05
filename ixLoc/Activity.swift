86554b0f9f0459c72329e3bf3543009b8a1d6efd
“Heyao <hy2nh@virginia.edu>
2017-07-05 16:56:02 +0200

    ForCharlesAndKavi

---
+++ ixLoc/Activity.swift
@@ -0,0 +1,9 @@
+//
+//  Activity.swift
+//  ixLoc
+//
+//  Created by super Tony on 7/4/17.
+//  Copyright © 2017 superTony. All rights reserved.
+//
+
+import Foundation
---
+++ ixLoc/AddActivityDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AddActivityDelegate.swift
+//  ixLoc
+//
+//  Created by super Tony on 7/5/17.
+//  Copyright © 2017 superTony. All rights reserved.
+//
+
+import Foundation
